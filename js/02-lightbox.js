53b23fae9aabafbe09c823b1fa78c99d171febed
Andrii Ursolov <andrew.ursolov@gmail.com>
2022-10-17 18:00:19 +0300

    update

---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('ul.gallery');
+
+galleryEl.addEventListener('click', onClickShowModal);
+
+function onClickShowModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  galleryEl.removeEventListener('click', onClickShowModal);
+  gallery.on('show.simplelightbox', function () {
+    gallery.options.captionDelay = 250;
+  });
+}
+
+function makeGallery(arr) {
+  const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}" /></a>`;
+    })
+    .join('');
+  return galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+}
+
+makeGallery(galleryItems);
+const gallery = new SimpleLightbox('ul.gallery a');
+console.log(galleryItems);
